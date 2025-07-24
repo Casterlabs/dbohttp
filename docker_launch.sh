f1530f883b53e84383f17ce653020a87a6c2c1aa
e3ndr <33337309+e3ndr@users.noreply.github.com>
2025-07-24 05:59:13 -0500

    Potentially fix interpolation issues.

--- docker_launch.sh
+++ docker_launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm /home/container/config.json
-echo '{"database": {"connectionString": "'"$DB_CONN_STR"'","driver": "'"$DB_DRIVER"'","accessTimeoutSeconds": 30},"debug": false,"jwtSecret": "'"$JWT_SECRET"'","port": 10243,"heartbeatUrl": null,"heartbeatIntervalSeconds": 15}' >> /home/container/config.json
+echo '{"database": {"connectionString": "'$DB_CONN_STR'","driver": "'$DB_DRIVER'","accessTimeoutSeconds": 30},"debug": false,"jwtSecret": "'$JWT_SECRET'","port": 10243,"heartbeatUrl": null,"heartbeatIntervalSeconds": 15}' >> /home/container/config.json
 
 java -XX:+CrashOnOutOfMemoryError -jar dbohttp.jar
